b9d4b0611449398948193e6d18e639eacb856682
Alan Barros <alan.frontd@gmail.com>
2023-05-31 20:45:30 -0300

    .

---
+++ CURSO DE JS AVANCADO/Revisao Logica 02/if_e_else.js
@@ -0,0 +1,16 @@
+const idade = 3
+
+
+
+if (idade >= 18)  {
+    console.log('Voce e maior de idade')
+} else if (idade > 13){
+    console.log('Voce e menor de idade')
+} else if (idade > 4){
+    console.log('Voce e crianca')
+} else{
+    console.log('Bebe')
+}
+
+
+const resultado = (6 == 6) ? "verdadeiro": "falso"
