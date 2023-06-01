78ae84b94205bf963b696261dd548090a11b4ee1
Alan Barros <alan.frontd@gmail.com>
2023-05-31 21:21:47 -0300

    .

---
+++ CURSO DE JS AVANCADO/Revisao Logica 02/Condicional_switch/switch.js
@@ -0,0 +1,29 @@
+let diasemana = 3;
+let dia = ``
+
+
+switch (diasemana){
+    case  0:
+        dia = `domingo`
+        break
+    case 1:
+        dia = `segunda`
+        break
+    case 2:
+        dia = `terca`
+        break
+    case 3:
+        dia = `quarta`
+        break
+    case 4:
+        dia = `quinta`
+        break
+    case 5:
+        dia = `sexta`
+        break
+    case 6:
+        dia = `sabado`
+        break
+}
+
+console.log(dia)
