739b2e398fb799a7dac7f2f37104986152129194
Alan Barros <alan.frontd@gmail.com>
2023-07-11 18:57:59 -0300

    .

---
+++ CURSO DE JS AVANCADO/DocumentObjectModel/aula 03/aula03.js
@@ -0,0 +1,28 @@
+function addContact(){
+    const contactSection = document.getElementById('contacts-list')
+
+    const h3 = document.createElement('h3')
+    h3.innerText = "Contato"
+
+    const ul = document.createElement('ul')
+
+    const nameLi = document.createElement('li')
+    nameLi.innerText = "Nome: "
+    const nameInput = document.createElement('input')
+    nameInput.type = 'text'
+    nameInput.name = 'fullname'
+    
+    nameLi.appendChild(nameInput)
+    ul.appendChild(nameLi)
+
+    const phoneLi = document.createElement('li')
+    phoneLi.innerText = "Telefone: "
+    const phoneInput = document.createElement('input')
+    phoneInput.type = 'phone'
+    phoneInput.name = 'Contato'
+    
+    phoneLi.appendChild(phoneInput)
+    ul.appendChild(phoneLi)
+    
+    contactSection.append(h3, ul)
+}
